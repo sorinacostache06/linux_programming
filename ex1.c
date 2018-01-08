2255a8e15b02fc7bf55935fdde93c3360d637e2a
sacostache <sorinacostache06@yahoo.com>
2018-01-08 10:48:23 +0200

    final producer multiconsumer

--- ex1.c
+++ ex1.c
@@ -4,13 +4,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <semaphore.h>
 
 #define GEN_TIME 1000000
+#define MAX_BUFF 1000
 
-int r = 0;
-pthread_cond_t condc = PTHREAD_COND_INITIALIZER;
-pthread_cond_t condp = PTHREAD_COND_INITIALIZER;
+int counter, r;
+int buffer[MAX_BUFF];
+
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+sem_t prod, cons;
 
 int isprime(int n)
 {
@@ -29,67 +33,82 @@ int isprime(int n)
     return 0;
 }
 
+int insert_item(int item)
+{
+    if (counter < MAX_BUFF) {
+        buffer[counter++] = item;
+        return 0;
+    }
+    return -1;
+}
+
+int remove_item(int *item)
+{
+    if (counter > 0) {
+        *item = buffer[counter - 1];
+        counter--;
+        return 0;
+    }
+    return -1;
+}
+
 void *producer(void *arg)
 {
-    for (int i = 0; i <= 3; i++) {
+    for (int i = 0; i <= 10; i++) {
+        sem_wait(&cons);
         pthread_mutex_lock(&mutex);
-        while (r != 0) 
-            pthread_cond_wait(&condp, &mutex);
-
+        
         srand(time(NULL));
         r = rand() % 1000 + 100;
-        pthread_cond_signal(&condc);
+        
         pthread_mutex_unlock(&mutex);
-        usleep(GEN_TIME);
+        sem_post(&prod);
+        sleep(GEN_TIME);
     }
     pthread_exit(0);
 }
 
 void *consumer(void *arg)
 {
-    for (int i = 0; i <= 3; i++) {
+    for (int i = 0; i <= 10; i++) {
+        sem_wait(&prod);
         pthread_mutex_lock(&mutex);
-
-        while (r == 0)
-            pthread_cond_wait(&condc, &mutex);
 
         if (isprime(r) == -1)
             printf("Thread <%ld>: number %d, prime: [NO]\n", pthread_self(), r);
         else
             printf("Thread <%ld>: number %d, prime: [YES]\n", pthread_self(), r);
 
-        r = 0;
-        pthread_cond_signal(&condp);
         pthread_mutex_unlock(&mutex);
-
+        sem_post(&cons);
     }
     pthread_exit(0);
 }
 
 int main()
 {
-    pthread_t p, c1, c2, c3;
-    int s, s1, s2, s3;
+    pthread_t p, c;
+    int s, s1;
+
+    if (sem_init(&prod, 0, 0) == -1)
+        printf("error init semaphore");
+    if (sem_init(&cons, 0, 1) == -1)
+        printf("error init semaphore");
 
     s = pthread_create(&p, NULL, producer, NULL);
     if (s != 0) 
         printf("error: threand was not created");
 
-    s1 = pthread_create(&c1, NULL, consumer, NULL);
-    s2 = pthread_create(&c2, NULL, consumer, NULL);
-    s3 = pthread_create(&c3, NULL, consumer, NULL);
-
-    // if (s != 0) 
-    //     printf("error: threand was not created");
-
+    for (int i = 0; i < 3; i++) {
+        s1 = pthread_create(&c, NULL, consumer, NULL);
+        if (s1 != 0) 
+            printf("error: threand was not created");
+    }
+        
     s = pthread_join(p, NULL);
-    s1 = pthread_join(c1, NULL);
-    s2 = pthread_join(c2, NULL);
-    s3 = pthread_join(c3, NULL);
-
+    for (int i = 0; i < 3; i++)
+        s1 = pthread_join(c, NULL);
 
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&condp);
-    pthread_cond_destroy(&condc);
     return 0;
 }
